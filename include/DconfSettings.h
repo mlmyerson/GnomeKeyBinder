3bfcef7f04b39d3811f3bb3658780f3302d5c24a
compzombie <mlmyerson@gmail.com>
2025-03-31 12:27:52 -0400

    created addKeybinding in DconfSettings

--- include/DconfSettings.h
+++ include/DconfSettings.h
@@ -9,32 +9,79 @@ namespace KeyBinder
 {
     class DconfSettings
     {
-    public:
-        DconfSettings();
-        ~DconfSettings();
 
     private:
-        std::string dconf_backup()
+        const std::string custom_keybindings_dot_schema_path = "org.gnome.settings-daemon.plugins.media-keys";
+        const std::string custom_keybindings_slash_schema_path = "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/";
+
+        std::string dconf_backup_store;
+
+        std::string dconfBackup()
         {
             const std::string cmd = "dconf dump /";
             std::string result = exec(cmd);
             return result;
         }
 
-        void dconf_restore()
+        void dconfRestore()
         {
             std::string temp_file = "/tmp/dconf_backup_" + std::to_string(getpid()) + ".ini";
             std::ofstream ofs(temp_file);
-            ofs << backup;
+            ofs << dconf_backup_store;
             ofs.close();
-            
+
             const std::string cmd = "dconf load / < " + temp_file;
             exec(cmd);
-            
+
             std::remove(temp_file.c_str());
         }
 
-        std::string backup;
+    public:
+        DconfSettings()
+        {
+            // Backup dconf settings
+            dconf_backup_store = dconfBackup();
+        }
+
+        ~DconfSettings();
+
+        std::string getCustomKeybindings()
+        {
+            std::string key_list = exec("gsettings get " + custom_keybindings_dot_schema_path + " custom-keybindings");
+
+            return key_list;
+        }
+
+        void addKeyBinding(const std::string &name, const std::string &key_command, const std::string &binding)
+        {
+            //get the current list
+            std::string bindings_list = getCustomKeybindings();
+
+            //check if first item in list            
+            std::string add_comma;
+            bindings_list == "[]" ? add_comma = "" : add_comma = ",";
+
+            std::string new_key = "custom_" + name;
+            //append new key to list
+            bindings_list.insert(bindings_list.length() - 1, add_comma + custom_keybindings_slash_schema_path + new_key + "/'");
+
+            //set the new list
+            std::string cmd = "gsettings set " + custom_keybindings_dot_schema_path + " custom-keybindings " + bindings_list;
+            exec(cmd);
+
+            //set name
+            cmd = "gsettings set " + custom_keybindings_dot_schema_path + " custom-keybindings:" + custom_keybindings_slash_schema_path + new_key + " name '" + name + "'";
+            exec(cmd);
+
+            //set command
+            cmd = "gsettings set " + custom_keybindings_dot_schema_path + " custom-keybindings:" + custom_keybindings_slash_schema_path + new_key + " name '" + key_command + "'";
+            exec(cmd);
+
+            //set keybinding
+            cmd = "gsettings set " + custom_keybindings_dot_schema_path + " custom-keybindings:" + custom_keybindings_slash_schema_path + new_key + " name '" + binding + "'";
+            exec(cmd);
+
+        }
     };
 }
 
