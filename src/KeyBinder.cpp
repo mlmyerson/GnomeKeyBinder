a845ac320d263c49f005250a8f7ca15f90ca938f
compzombie <mlmyerson@gmail.com>
2025-04-27 08:11:34 -0400

    made a special adjusted path

--- src/KeyBinder.cpp
+++ src/KeyBinder.cpp
@@ -35,7 +35,8 @@ std::string GnomeKeyBinder::KeyBinder::getPathByName(const std::string &name) co
 
 std::string GnomeKeyBinder::KeyBinder::getCustomKeysPath() const
 {
-    std::string cmd = "gsettings get " + dot_schema_path + " custom-keybindings";
+    std::string adjusted_path = remove_first_instance(dot_schema_path, ".custom-keybindings");
+    std::string cmd = "gsettings get " + adjusted_path + " custom-keybindings";
     std::string result = exec(cmd);
     result = remove_first_instance(result, "@as");
     return result;
